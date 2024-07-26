23cf86db2c9db22b02262865b609f69e51f35868
maozkurt <maozkurt12@gmail.com>
2024-07-26 10:46:34 +0300

    namespace, device class & constructor

--- i2c.cpp
+++ i2c.cpp
@@ -5,36 +5,39 @@
             register to be read is 0x01.
 
 */
-#include <iostream>
-#include <cstdint>
-extern "C"{
-    #include <linux/i2c-dev.h>
-    #include <i2c/smbus.h>
-    #include <fcntl.h>
-    #include <sys/ioctl.h>
+#include "i2c.hpp"
+
+
+i2c::device::device(int i2cdeviceno,int deviceaddress){
+    i2cfilepath.append(std::to_string(i2cdeviceno));
+    this->deviceaddress = deviceaddress;
+    
+    this->fileno = open(i2cfilepath.c_str(), O_RDWR);
+    if(fileno < 0){
+        std::cerr << "Error while opening " <<  i2cfilepath << std::endl;
+        exit(1);
+    }
+    if(ioctl(this->fileno, I2C_SLAVE, deviceaddress) < 0){
+        std::cerr << "Error" << std::endl;
+    }
 }
 
-char i2cdevice[] = "/dev/i2c-0";
+i2c::device::~device(){
+
+}
 
 int main(){
     // Using open to be async-signal-safe
-    int fileno = open(i2cdevice, O_RDWR);
-    if(fileno < 0){
-        std::cerr << "Error while opening " <<  i2cdevice << std::endl;
-        exit(1);
-    }
-
-    int32_t deviceaddress = 0x08;
-    if(ioctl(fileno, I2C_SLAVE, deviceaddress) < 0){
-        std::cerr << "Error" << std::endl;
-    }
-
+    auto a = i2c::device(0,0x08);
+    a.test();
+    
     uint8_t reg2access = 0x01;
     int64_t ret;
-    ret = i2c_smbus_read_word_data(fileno, reg2access);
-    std::cout  << (int)ret << std::endl;
+    ret = i2c_smbus_read_word_data(a.getfileno(), reg2access);
+    //ret = i2c_smbus_write_word_data(a.getfileno(), reg2access,0x3A);
+    std::cout  << std::hex << (int)ret << std::endl;
     if(ret < 0){
         std::cerr << "Error in i2c transaction" << std::endl;
     }
-
+    
 }
---
+++ i2c.hpp
@@ -0,0 +1,30 @@
+#ifndef _I2C_HPP
+#define _I2C_HPP
+
+#include <iostream>
+#include <cstdint>
+#include <string>
+extern "C"{
+    #include <linux/i2c-dev.h>
+    #include <i2c/smbus.h>
+    #include <fcntl.h>
+    #include <sys/ioctl.h>
+}
+
+namespace i2c {
+    class device {
+    private:
+        std::string  i2cfilepath = "/dev/i2c-";
+        int deviceaddress;
+        int fileno;
+    public:
+        device(int i2cdeviceno, int deviceaddress);
+        ~device();
+        int getfileno(){return fileno;};
+        void test(){
+            std::cout << i2cfilepath << ' ' << deviceaddress << std::endl;
+        }
+    };
+}
+
+#endif
